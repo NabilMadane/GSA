dcdf493f904f934a3b987956ca4dd83b4790b29e
madanenabil <madanenabil@gmail.com>
2025-02-25 18:33:55 +0000

    add logo

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -103,7 +103,7 @@
         <section class="sidebar">
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu" data-widget="tree">
-            <li class="header">MAIN NAVIGATION</li>
+            <li class="header">NAVIGATION PRINCIPALE</li>
             <li>
               <a href="<?php echo base_url(); ?>dashboard">
                 <i class="fa fa-dashboard"></i> <span>Tableau de bord</span></i>
